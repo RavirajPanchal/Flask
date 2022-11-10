c6f1983906d952e4857c169c37b404bcd1879243
Raviraj Panchal <raviraj.panchal@mphasis.com>
2022-11-10 12:14:01 +0530

    changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+""" from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/main')
@@ -24,4 +24,41 @@ if __name__=="__main__":
 
 @app.route('/about/<username>')
 def about_page(username):
+        return f'<h1> this the about page of {username}</h1>' """
+
+
+from flask import Flask,render_template
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+
+def home():
+
+    return render_template("home.html")
+
+   
+
+@app.route("/index")
+
+def index():
+
+    return render_template("index.html")
+
+
+@app.route('/about/<username>')
+def about_page(username):
         return f'<h1> this the about page of {username}</h1>'
+
+
+@app.route('/product')
+def product():
+    return"<h1>Select your product<h1>"
+
+
+if __name__=='__main__':
+ app.run(debug = True)
